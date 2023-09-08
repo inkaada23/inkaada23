73aaac57c711b362282961cc73b51c3e52528fda
inkaada23 <125627048+inkaada23@users.noreply.github.com>
2023-09-08 17:12:24 -0400

    Add files via upload

---
+++ School python.py
@@ -0,0 +1,38 @@
+#
+print(4)
+print("Hello, World!")
+print(type("Hello, World!"))
+print(type('Hello, World!'))
+print(type(17))
+print(type(3.2))
+print(type('17'))
+print(type('3.2'))
+print(1,000,000)
+print(1_000_000)
+message = 'And now for something different'
+n = 17
+pi = 3.14
+print(message)
+print(n)
+print(pi)
+message = 'Some string'
+print(type(message))
+n = 17
+print(type(n))
+n = 1.7
+print(type(n))
+print(1)
+x = 2
+print(x)
+
+
+
+
+print(20 + 32)
+year = 2020
+print("Last year was", year - 1)
+price = 20.65
+tax_rate = .06
+print("tax is", price * tax_rate)
+print(90/60)
+print(5 ** 2)
